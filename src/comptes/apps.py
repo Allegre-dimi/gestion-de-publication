a0d3109ea6cf7f303eb29f39537a0fd3ff4e3edd
elshadall <dimiallegre@gmail.com>
2023-05-06 14:38:51 +0100

    modif de l'appli comptes

--- src/comptes/apps.py
+++ src/comptes/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class GestionComptesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'gestion_comptes'
+    name = 'comptes'
