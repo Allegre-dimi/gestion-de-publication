cc628ed967b8b08677ae2429b2ddb6d3ec56505d
elshadall <dimiallegre@gmail.com>
2023-07-23 14:55:38 +0100

    modification de l'arboressance du projet

--- src/comptes/models.py
+++ src/comptes/models.py
@@ -1,5 +1,40 @@
+import bcrypt
+from django.core import validators
 from django.db import models
 
 # Create your models here.
+
+class Utilisateur(models.Model):
+    nom = models.CharField(max_length=100)
+    prenom = models.CharField(max_length=100)
+    email = models.EmailField()
+    telephone = models.CharField(max_length=100)
+    photo = models.ImageField(upload_to="generer_nom_fichier", blank=True, null=True)
+
+    def __str__(self):
+        return self.nom
+    
+class Comptes(models.Model):
+    login = models.CharField(max_length=100)
+    password = models.CharField(max_length=128, blank= False, unique = True)
+
+    def __str__(self):
+        return self.login
+    
+class typeProfile(models.Model):
+
+    def __str__(self):
+        return self
+
+
+
+
+    
+    
+    
+
+
+
+
 
 
